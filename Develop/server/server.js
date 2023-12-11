ad543b2d4ed3c5aaa6023ab46ff2d61b1b380241
jannecook <135638400+jannecook@users.noreply.github.com>
2023-12-10 23:27:01 -0800

    update server.js file to pull in apollo and graphql

--- Develop/server/server.js
+++ Develop/server/server.js
@@ -1,21 +1,38 @@
 const express = require('express');
-const path = require('path');
+
+//Import ApolloServer and helper function
+const { ApolloServer } = require('@apollo/server');
+const { expressMiddleware } = require('@apollo/server/express4');
+
+//Import GraphQL schemas
+const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+
+const PORT = process.env.PORT || 3001;
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+});
 
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// New Apollo server instance using GraphQL schema
+const startApolloServer = async () => {
+  await server.start();
+  
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+  app.use('/graphql', expressMiddleware(server));
 
-app.use(routes);
+  db.once('open', () => {
+    app.listen(PORT, () => {
+      console.log(`API server running on port ${PORT}!`);
+      console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+    })
+  })
+};
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// Call async function to start server
+startApolloServer();
+
